c5c8e5146c2707381658f793edca426c91d948a6
HE Qichen <heqichen@gmail.com>
2023-05-05 22:17:26 +0800

    remove test temporary

--- cbcenter/src/test/gear/index_test.ts
+++ cbcenter/src/test/gear/index_test.ts
@@ -3,11 +3,11 @@ import GearManager from "../../gear/index";
 const gm: GearManager = new GearManager();
 
 
-gm.onGearConnected((name: string) => {
-    console.log("[", name, "] connected");
-});
+// gm.onGearConnected((name: string) => {
+//     console.log("[", name, "] connected");
+// });
 
-gm.begin().then(() => { }).catch((e) => { });
+// gm.begin().then(() => { }).catch((e) => { });
 
-// Test interface
-gm.listGearNames();
+// // Test interface
+// gm.listGearNames();
